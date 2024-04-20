520c3f2041d7241487d62ff22b2c606699102020
Ontu <73860587+0ntu@users.noreply.github.com>
2024-04-20 14:19:39 -0400

    Starship ssh remote design

--- home/cli/starship.nix
+++ home/cli/starship.nix
@@ -3,8 +3,8 @@
     enable = true;
     settings = {
       hostname = {
-        ssh_symbol = "󰢹";
-        format = "[$ssh_symbol$hostname]($style)";
+        ssh_symbol = " 󰢹";
+        format = "[$hostname]($style)[$ssh_symbol](peach)";
         ssh_only = false;
         style = "bold blue";
       };
