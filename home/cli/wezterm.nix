5a88f585aca5362a8ca709114396553c2e598db2
Ontu <73860587+0ntu@users.noreply.github.com>
2024-04-20 15:28:52 -0400

    index on master: 10f4c37 Starship ssh remote design

--- home/cli/wezterm.nix
+++ home/cli/wezterm.nix
@@ -6,6 +6,7 @@
       local wezterm = require 'wezterm'
       return {
         window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+        enable_wayland = false,
         color_scheme = "Catppuccin Mocha",
         audible_bell = "Disabled",
         enable_tab_bar = false,
