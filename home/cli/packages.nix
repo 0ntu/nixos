8ad6b5d8087d38734305f27022d2c21791f2bc95
Ontu <73860587+0ntu@users.noreply.github.com>
2024-08-24 22:39:54 -0400

    zip should be a system pkg

--- home/cli/packages.nix
+++ home/cli/packages.nix
@@ -5,7 +5,6 @@
   home.packages = with pkgs; [
     alejandra
     lazygit
-    unzip
     nvd
     nh
     bat
--- system/core/devpkgs.nix
+++ system/core/devpkgs.nix
@@ -1,5 +1,8 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
+    zip
+    unzip
+
     git
     curl
     wget
