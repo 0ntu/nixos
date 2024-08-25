cbc727dc3d8f1b650f856bae7acbdfb65589c567
Ontu <73860587+0ntu@users.noreply.github.com>
2024-08-25 16:12:34 -0400

    Failed zellij keybinds

--- home/cli/zellij.nix
+++ home/cli/zellij.nix
@@ -4,6 +4,15 @@
     enableZshIntegration = true;
     settings = {
       theme = "catppuccin-mocha";
+      pane_frames = false;
+      keybinds.normal = {
+        # "bind r\"Ctrl \\\"" = {
+        #   ToggleFloatingPanes = {};
+        #   SwitchToMode = {
+        #     _args = [ "normal" ];
+        #   };
+        # };
+      };
     };
   };
 }
