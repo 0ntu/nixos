adcf6693b70d1bb0c07d7e6ec56b92b6d3c03437
Ontu <73860587+0ntu@users.noreply.github.com>
2024-09-10 13:13:25 -0400

    Update ssh keys

--- system/core/users.nix
+++ system/core/users.nix
@@ -7,8 +7,7 @@
       shell = pkgs.zsh;
       extraGroups = ["networkmanager" "wheel" "libvirtd"];
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUmrKruT4g5JOAPCFTVCT0rEYSntjyU9Omb473dvw96 vim@debian"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIImIt8UHkzhzsYrUTU6c8gM19Z3pqtk89Wp9dvhJsQJY ontu@ontu-framework"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINEeuNKcX3LhltjiwWdHmrD2h78w4vQxrv9k0aZxkb+X framework"
       ];
     };
   };
