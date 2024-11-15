864b91b1ec0785ef7fb15c20fb5bd00402dddb90
Ontu <73860587+0ntu@users.noreply.github.com>
2024-11-15 05:14:13 -0500

    readd lightdm & nvidia

--- system/virt.nix
+++ system/virt.nix
@@ -1,4 +1,10 @@
 {...}: {
+  # host
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
+
+  # guest
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.clipboard = true;
+  virtualisation.virtualbox.guest.dragAndDrop = true;
 }
