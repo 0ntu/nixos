9c7812f70d954df7ad8f739c25eee2f1841284be
Ontu <73860587+0ntu@users.noreply.github.com>
2024-12-06 13:43:03 -0500

    remove guest virtualbox

--- system/virt.nix
+++ system/virt.nix
@@ -4,7 +4,7 @@
   programs.virt-manager.enable = true;
 
   # guest
-  virtualisation.virtualbox.guest.enable = true;
-  virtualisation.virtualbox.guest.clipboard = true;
-  virtualisation.virtualbox.guest.dragAndDrop = true;
+  # virtualisation.virtualbox.guest.enable = true;
+  # virtualisation.virtualbox.guest.clipboard = true;
+  # virtualisation.virtualbox.guest.dragAndDrop = true;
 }
