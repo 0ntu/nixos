295ddcf916343b497e1dca95c5fe58623bc4d4dd
Ontu <73860587+0ntu@users.noreply.github.com>
2024-12-31 13:30:55 -0500

    virsh environment var

--- system/virt.nix
+++ system/virt.nix
@@ -2,6 +2,8 @@
   # host
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
+  # let virsh connect to system qemu (allows config through virt-manager & virsh)
+  environment.variables = { LIBVIRT_DEFAULT_URI = "qemu:///system"; };
 
   # guest
   # virtualisation.virtualbox.guest.enable = true;
