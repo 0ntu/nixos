52e58b72d8dbe5470e2dd39eddf3592408a25323
Ontu <73860587+0ntu@users.noreply.github.com>
2024-12-31 15:53:56 -0500

    bat themes comment

--- home/cli/zsh/tools.nix
+++ home/cli/zsh/tools.nix
@@ -1,3 +1,14 @@
-{...}: {
-  programs.bat.enable = true;
+{pkgs, ...}: {
+  programs.bat = {
+    enable = true;
+    # themes = {
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "catppuccin";
+    #     repo = "bat"; # 
+    #     rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
+    #     sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
+    #   };
+    #   file = "Catppuccin Mocha.tmTheme";
+    # };
+  };
 }
