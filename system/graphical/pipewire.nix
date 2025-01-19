de771e52a42baa516f288fbdcc89f81d0d3fdd20
Ontu <73860587+0ntu@users.noreply.github.com>
2025-01-19 13:59:20 -0500

    Fix pipewire crackling

--- system/graphical/pipewire.nix
+++ system/graphical/pipewire.nix
@@ -5,6 +5,14 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.allowed-rates" = [ 44100 48000 96000 ];
+        "default.clock.quantum" = 32;
+        "default.clock.min-quantum" = 32;
+        "default.clock.max-quantum" = 1024;
+      };
+    };
   };
   hardware.pulseaudio.enable = false;
 }
