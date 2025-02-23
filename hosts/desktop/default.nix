908c8ef04f40153783bf655a63bb2fb6bf489266
Ontu <73860587+0ntu@users.noreply.github.com>
2025-02-22 23:59:41 -0500

    add vmware + openssh allowed users

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,7 +1,16 @@
-{...}: {
+{pkgs-stable, ...}: {
   imports = [
     ./hardware-configuration.nix
   ];
   networking.firewall.enable = false;
   services.openssh.enable = true;
+  virtualisation.vmware.host = {
+    enable = true;
+  };
+
+  services.openssh.settings = {
+    AllowUsers = [ "ontu" ];
+  };
+
+
 }
