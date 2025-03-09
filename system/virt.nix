cd68695b3a09632545e1ed0edb143d410c05b597
Ontu <73860587+0ntu@users.noreply.github.com>
2025-03-09 18:05:26 -0400

    remove docker!!

--- system/virt.nix
+++ system/virt.nix
@@ -5,11 +5,7 @@
     qemu.vhostUserPackages = with pkgs; [virtiofsd];
   };
   programs.virt-manager.enable = true;
+
   # let virsh connect to system qemu (allows config through virt-manager & virsh)
   environment.variables = {LIBVIRT_DEFAULT_URI = "qemu:///system";};
-
-  virtualisation.docker = {
-    enable = true;
-  };
-
 }
