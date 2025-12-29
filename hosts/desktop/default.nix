d1b2226009c23ef39f9043c0379b9ce8f3f498d2
Ontu <73860587+0ntu@users.noreply.github.com>
2025-12-28 23:18:52 -0500

    disable sunshine

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -26,16 +26,16 @@
     enable = true;
   };
 
-  services.sunshine = {
-    enable = true;
-  };
-
-  security.wrappers.sunshine = {
-    owner = "root";
-    group = "root";
-    capabilities = "cap_sys_admin+p";
-    source = "${pkgs.sunshine}/bin/sunshine";
-  };
+  # services.sunshine = {
+  #   enable = true;
+  # };
+  #
+  # security.wrappers.sunshine = {
+  #   owner = "root";
+  #   group = "root";
+  #   capabilities = "cap_sys_admin+p";
+  #   source = "${pkgs.sunshine}/bin/sunshine";
+  # };
 
   environment.systemPackages = with pkgs; [
     prismlauncher
