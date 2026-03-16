69de1d5bcc890e61cfa5ec221707bc12943b8c2c
Ontu <73860587+0ntu@users.noreply.github.com>
2026-03-16 00:26:04 -0400

    wezterm update

--- packages/wezterm/config.lua
+++ packages/wezterm/config.lua
@@ -10,6 +10,7 @@ config.hide_tab_bar_if_only_one_tab = true
 -- config.font = wezterm.font 'Cousine'
 config.font_size = 10.0
 config.default_cursor_style = 'SteadyBlock'
+config.use_fancy_tab_bar = false;
 
 config.keys = {
   {
@@ -58,9 +59,89 @@ config.keys = {
     action = wezterm.action.CloseCurrentPane { confirm = false },
   },
   {
+    key = 't',
+    mods = 'ALT',
+    action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+  },
+  {
     key = 'q',
     mods = 'CTRL|SHIFT',
     action = wezterm.action.CloseCurrentPane { confirm = false },
+  },
+  {
+    key = 'j',
+    mods = 'CTRL',
+    action = wezterm.action.ScrollByPage(1),
+  },
+  {
+    key = 'k',
+    mods = 'CTRL',
+    action = wezterm.action.ScrollByPage(-1),
+  },
+  {
+    key = '1',
+    mods = 'ALT',
+    action = wezterm.action.ActivateTab(0),
+  },
+  {
+    key = '2',
+    mods = 'ALT',
+    action = wezterm.action.ActivateTab(1),
+  },
+  {
+    key = '3',
+    mods = 'ALT',
+    action = wezterm.action.ActivateTab(2),
+  },
+  {
+    key = '4',
+    mods = 'ALT',
+    action = wezterm.action.ActivateTab(3),
+  },
+  {
+    key = '5',
+    mods = 'ALT',
+    action = wezterm.action.ActivateTab(4),
+  },
+  {
+    key = '6',
+    mods = 'ALT',
+    action = wezterm.action.ActivateTab(5),
+  },
+  {
+    key = '7',
+    mods = 'ALT',
+    action = wezterm.action.ActivateTab(6),
+  },
+  {
+    key = '8',
+    mods = 'ALT',
+    action = wezterm.action.ActivateTab(7),
+  },
+  {
+    key = '9',
+    mods = 'ALT',
+    action = wezterm.action.ActivateTab(8),
+  },
+  {
+    key = 'h',
+    mods = 'ALT|SHIFT',
+    action = wezterm.action.ActivateTabRelative(-1),
+  },
+  {
+    key = 'l',
+    mods = 'ALT|SHIFT',
+    action = wezterm.action.ActivateTabRelative(1),
+  },
+  {
+    key = 'j',
+    mods = 'ALT|SHIFT',
+    action = wezterm.action.MoveTabRelative(-1),
+  },
+  {
+    key = 'k',
+    mods = 'ALT|SHIFT',
+    action = wezterm.action.MoveTabRelative(1),
   },
 }
 
